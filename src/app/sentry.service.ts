623d7b429834e56f49187bfd718744b78108d3e8
Preston Lamb <preston.j.lamb@gmail.com>
2019-04-11 19:15:00 -0600

    only report to sentry in production

--- src/app/sentry.service.ts
+++ src/app/sentry.service.ts
@@ -1,15 +1,20 @@
 import { Injectable, ErrorHandler } from '@angular/core';
 import * as Sentry from '@sentry/browser';
+import { environment } from 'src/environments/environment';
 
-Sentry.init({
-	dsn: 'https://c906225ee45b4e9d9f2b38a2742a0c2c@sentry.io/1414944',
-});
+if (environment.production) {
+	Sentry.init({
+		dsn: 'https://c906225ee45b4e9d9f2b38a2742a0c2c@sentry.io/1414944',
+	});
+}
 
 @Injectable()
 export class SentryErrorHandler implements ErrorHandler {
 	constructor() {}
 	handleError(error) {
-		Sentry.captureException(error.originalError || error);
+		if (environment.production) {
+			Sentry.captureException(error.originalError || error);
+		}
 		throw error;
 	}
 }
